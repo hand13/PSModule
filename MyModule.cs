fc8d867eb5f96fd8fd008e5672da2822ca81379e
hand13 <hand13@qq.com>
2021-01-06 10:37:26 +0800

    myModule

---
+++ MyModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace MyModule
+{
+    [Cmdlet(VerbsCommunications.Write,"TimestampedMessage")]
+    public class WriteTimestampedMessageCommand:PSCmdlet
+    {
+        [Parameter(Position = 1)]
+        public string Message {get;set;} = string.Empty;
+        [Parameter(Position = 2)]
+        public int time{get;set;} = 1;
+        [Parameter(Mandatory = false)]
+        public Boolean greet = false;
+        protected override void EndProcessing()
+        {
+            string timestamp = DateTime.Now.ToString();
+            if(greet) {
+                this.WriteObject("no you die");
+            }
+            for(int i = 0;i<time;i++) {
+                this.WriteObject($"[{timestamp}] - {this.Message}");
+            }
+            base.EndProcessing();
+        }
+    }
+}
